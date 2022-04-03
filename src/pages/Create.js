d2327aee712949e7d35ce13814e545c0c18980b3
CodexDev <stefanga2020@gmail.com>
2022-04-03 13:50:44 +0300

    Intialized the Form Submit process

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -1,5 +1,82 @@
-import React from "react";
+import { React, useState, useContext, useRef } from "react";
+import { Typography, Button, Container, TextField } from "@mui/material";
+import KeyboardArrowRightIcon from "@mui/icons-material/KeyboardArrowRight";
 
 export default function Create() {
-  return <div>Create</div>;
+  const [title, setTitle] = useState("");
+  const [details, setDetails] = useState("");
+
+  const [titleError, setTitleError] = useState(false);
+  const [detailsError, setDetailsError] = useState(false);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    setTitleError(false);
+    setDetailsError(false);
+
+    if (title == "") {
+      setTitleError(true);
+    }
+
+    if (details == "") {
+      setDetailsError(true);
+    }
+
+    if (title && details) {
+      console.log(title, details);
+    }
+  };
+
+  return (
+    <Container>
+      <Typography
+        variant="h6"
+        color="textSecondary"
+        component="h2"
+        gutterBottom="true"
+      >
+        Create a new Note
+      </Typography>
+
+      <form noValidate autoComplete="off" onSubmit={handleSubmit}>
+        <TextField
+          onChange={(e) => {
+            setTitle(e.target.value);
+          }}
+          sx={{
+            marginBottom: 2,
+            marginTop: 2,
+          }}
+          label="Note Title"
+          fullWidth
+          required
+          error={titleError}
+        />
+        <TextField
+          onChange={(e) => {
+            setDetails(e.target.value);
+          }}
+          sx={{
+            marginBottom: 2,
+          }}
+          label="Details"
+          multiline
+          rows={4}
+          fullWidth
+          required
+          error={detailsError}
+        />
+
+        <Button
+          variant="contained"
+          color="primary"
+          type="submit"
+          endIcon={<KeyboardArrowRightIcon />}
+        >
+          Submit
+        </Button>
+      </form>
+    </Container>
+  );
 }
